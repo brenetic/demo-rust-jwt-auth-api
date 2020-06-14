4faa3002d74922f5fd3fc7f74a7788be01706115
brenetic <b@brenetic.com>
2020-06-14 22:58:38 +0100

    Add authorisation module

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,13 @@
 extern crate diesel;
 
 use actix_web::{dev::ServiceRequest, web, App, Error, HttpServer};
+use actix_web_httpauth::extractors::bearer::{BearerAuth, Config};
+use actix_web_httpauth::extractors::AuthenticationError;
+use actix_web_httpauth::middleware::HttpAuthentication;
 use diesel::prelude::*;
 use diesel::r2d2::{self, ConnectionManager};
 
+mod auth;
 mod errors;
 mod handlers;
 mod models;
@@ -27,7 +31,9 @@ async fn main() -> std::io::Result<()> {
 
     // Start http server
     HttpServer::new(move || {
+        let auth = HttpAuthentication::bearer(validator);
         App::new()
+            .wrap(auth)
             .data(pool.clone())
             .route("/users", web::get().to(handlers::get_users))
             .route("/users/{id}", web::get().to(handlers::get_user_by_id))
@@ -37,4 +43,21 @@ async fn main() -> std::io::Result<()> {
     .bind("127.0.0.1:8080")?
     .run()
     .await
+}
+
+async fn validator(req: ServiceRequest, credentials: BearerAuth) -> Result<ServiceRequest, Error> {
+    let config = req
+        .app_data::<Config>()
+        .map(|data| data.get_ref().clone())
+        .unwrap_or_else(Default::default);
+    match auth::validate_token(credentials.token()) {
+        Ok(res) => {
+            if res == true {
+                Ok(req)
+            } else {
+                Err(AuthenticationError::from(config).into())
+            }
+        }
+        Err(_) => Err(AuthenticationError::from(config).into()),
+    }
 }
